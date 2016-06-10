cad83c03166fca9aa8170e49fdd4541b5daabb93
igeligel <kevinigeligeligel@gmail.com>
2016-06-10 16:23:44 +0200

    feat(test)
    add first test method

---
+++ src/csharp-steamgaug-es-api-test/Integration/WebTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csharp_steamgaug_es_api_test.Integration
+{
+    [TestClass]
+    public class WebTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
