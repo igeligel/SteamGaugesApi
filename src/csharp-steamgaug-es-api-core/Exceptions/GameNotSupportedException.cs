fe9d9f7a56bfebd8cecf9f4be0b4a3ec5bbc7086
igeligel <kevinigeligeligel@gmail.com>
2016-06-10 12:48:20 +0200

    feat(exception)
    add game not supported exception

---
+++ src/csharp-steamgaug-es-api-core/Exceptions/GameNotSupportedException.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp_steamgaug_es_api_core.Exceptions
+{
+    public class GameNotSupportedException : Exception
+    {
+    }
+}
