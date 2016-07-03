e0e92940556aa4a564102eb65be24666cf3e43b3
igeligel <kevinigeligeligel@gmail.com>
2016-07-04 00:22:35 +0200

    chore(appveyor)
    trigger

--- build.ps1
+++ build.ps1
@@ -58,9 +58,9 @@ EnsurePsbuildInstalled
 exec { & dotnet restore }
 
 Invoke-MSBuild
-
+<#
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)#>
 
 exec { & dotnet test ..\csharp-steamgaug-es-api-test\ }
 <# -c release normally #>
