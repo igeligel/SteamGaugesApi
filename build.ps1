ae09ac8533c3e2f91e33d765d6805602580b6b03
igeligel <kevinigeligeligel@gmail.com>
2016-07-04 00:17:41 +0200

    chore(appveyor)
    edit script for test

--- build.ps1
+++ build.ps1
@@ -62,5 +62,6 @@ Invoke-MSBuild
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet test ..\csharp-steamgaug-es-api-test -c Release }
+exec { & dotnet test ..\csharp-steamgaug-es-api-test\ }
+<# -c release normally #>
 <# exec { & dotnet pack .\src\MediatR -c Release -o .\artifacts --version-suffix=$revision } #>
