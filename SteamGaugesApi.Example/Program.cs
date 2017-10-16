db3f7712f18f41361e19a43b0462f65bc0fd15b9
igeligel <kevinigeligeligel@gmail.com>
2017-10-16 22:08:13 +0200

    chore(project)
    setup project environment for .NET Standard 2

---
+++ SteamGaugesApi.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SteamGaugesApi.Example
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
---
+++ SteamGaugesApi.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SteamGaugesApi.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
