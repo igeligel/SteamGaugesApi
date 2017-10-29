262920e5f2e1ff3ed8721f373ec131988531adf7
igeligel <kevinigeligeligel@gmail.com>
2017-10-29 13:20:56 +0100

    fix(client): extend client
    extend client by providing an own http client handler which is important
    for testing

--- SteamGaugesApi.Core/Client.cs
+++ SteamGaugesApi.Core/Client.cs
@@ -6,10 +6,21 @@ namespace SteamGaugesApi.Core
 {
     public class Client
     {
+        private readonly HttpClient _httpClient;
+
+        public Client()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public Client(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public SteamGaugesResponse Get()
         {
-            var httpClient = new HttpClient();
-            var responseContent = httpClient.GetAsync("https://steamgaug.es/api/v2").Result
+            var responseContent = _httpClient.GetAsync("https://steamgaug.es/api/v2").Result
                 .Content.ReadAsStringAsync().Result;
             return JsonConvert.DeserializeObject<SteamGaugesResponse>(responseContent);
         }
