5937b734b0a2ff5d23e85ce918d46a7d669edcb6
igeligel <kevinigeligeligel@gmail.com>
2017-10-29 14:20:59 +0100

    test(project)
    update to ignore some projects

--- .scripts/TestCoverage.bat
+++ .scripts/TestCoverage.bat
@@ -2,7 +2,7 @@
 
 IF DEFINED APPVEYOR (
     REM dotnet test SteamGaugesApi.Test\SteamGaugesApi.Test.csproj -c Debug --no-build --logger \"trx;LogFileName=%APPVEYOR_BUILD_FOLDER%\TestResults.trx\"
-    C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe -register:user -target:"c:\Program Files\dotnet\dotnet.exe" -targetargs:"test SteamGaugesApi.Test\SteamGaugesApi.Test.csproj -c Debug --no-build --logger \"trx;LogFileName=%APPVEYOR_BUILD_FOLDER%\TestResults.trx\" " -threshold:1 -oldStyle -returntargetcode -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\*Designer.cs -hideskipped:All -mergebyhash -mergeoutput -output:.\coverage-dotnet.xml
+    C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe -register:user -target:"c:\Program Files\dotnet\dotnet.exe" -targetargs:"test SteamGaugesApi.Test\SteamGaugesApi.Test.csproj -c Debug --no-build --logger \"trx;LogFileName=%APPVEYOR_BUILD_FOLDER%\TestResults.trx\" " -threshold:1 -oldStyle -returntargetcode -filter:"+[SteamGaugesApi*]* -[SteamGaugesApi*.Test]* -[SteamGaugesApi*.Example]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\*Designer.cs -hideskipped:All -mergebyhash -mergeoutput -output:.\coverage-dotnet.xml
     REM C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe -register:user -target:"%xunit20%\xunit.console.x86.exe" -targetargs:"SteamGaugesApi.Test\bin\Debug\netcoreapp2.0\SteamGaugesApi.Test.dll -noshadow -appveyor -xml .\xunit-results.xml" -threshold:1 -oldStyle -returntargetcode  -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\*Designer.cs -hideskipped:All -mergebyhash -mergeoutput -output:.\coverage-dotnet.xml
 ) ELSE (
 	echo No coverage without appveyor.
