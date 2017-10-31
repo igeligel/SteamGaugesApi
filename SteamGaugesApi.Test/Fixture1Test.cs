40e49628b2f92604a3af9feeadc74c4ff445112c
igeligel <kevinigeligeligel@gmail.com>
2017-10-31 17:24:32 +0100

    test(fixture1): complete tests
    complete fixture test by adding tests for every property

--- SteamGaugesApi.Test/Fixture1Test.cs
+++ SteamGaugesApi.Test/Fixture1Test.cs
@@ -1,9 +1,12 @@
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SteamGaugesApi.Core;
+using SteamGaugesApi.Core.Models;
 
 namespace SteamGaugesApi.Test
 {
@@ -224,6 +227,308 @@ namespace SteamGaugesApi.Test
                     .EconomyItemsInterFace
                     .CounterStrikeGlobalOffensiveEconomyInterface
                     .Error
+            );
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorOnline()
+        {
+            var response = _client.Get();
+            Assert.IsTrue(
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+                    .Online
+            );
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorEmptySchema()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                "",
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+                    .Schema
+            );
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorNoError()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                "No Error",
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+                    .Error
+            );
+        }
+
+        private static Score GetWarScore(
+            GameCoordinatorInterface gameCoordinatorInterface, int side
+        )
+        {
+            return
+                gameCoordinatorInterface
+                    .Statistics
+                    .WarScore
+                    .FirstOrDefault(
+                        e => e.Side == side
+                    )
+                    ?.Score;
+        }
+
+        private static Score GetFirstWarScore(
+            GameCoordinatorInterface gameCoordinatorInterface
+        )
+        {
+            return GetWarScore(gameCoordinatorInterface, 0);
+        }
+
+        private static Score GetSecondWarScore(
+            GameCoordinatorInterface gameCoordinatorInterface
+        )
+        {
+            return GetWarScore(gameCoordinatorInterface, 1);
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorFirstWarScoreLow()
+        {
+            var response = _client.Get();
+            var firstSideScore = GetFirstWarScore(
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+            );
+            Assert.AreEqual(5072919, firstSideScore.Low);
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorFirstWarScoreHigh()
+        {
+            var response = _client.Get();
+            var firstSideScore = GetFirstWarScore(
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+            );
+            Assert.AreEqual(0, firstSideScore.High);
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorFirstWarScoreIsUnsigned()
+        {
+            var response = _client.Get();
+            var firstSideScore = GetFirstWarScore(
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+            );
+            Assert.IsTrue(firstSideScore.Unsigned);
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorSecondWarScoreLow()
+        {
+            var response = _client.Get();
+            var firstSideScore = GetSecondWarScore(
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+            );
+            Assert.AreEqual(6422457, firstSideScore.Low);
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorSecondWarScoreHigh()
+        {
+            var response = _client.Get();
+            var firstSideScore = GetSecondWarScore(
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+            );
+            Assert.AreEqual(0, firstSideScore.High);
+        }
+
+        [TestMethod]
+        public void TeamFortressGameCoordinatorSecondWarScoreIsUnsigned()
+        {
+            var response = _client.Get();
+            var firstSideScore = GetSecondWarScore(
+                response
+                    .SteamGameCoordinationInterface
+                    .TeamFortressGameCoordinatorInterface
+            );
+            Assert.IsTrue(firstSideScore.Unsigned);
+        }
+
+        [TestMethod]
+        public void DotaGameCoordinatorIsOnline()
+        {
+            var response = _client.Get();
+            Assert.IsTrue(
+                response
+                    .SteamGameCoordinationInterface
+                    .DotaGameCoordinatorInterface
+                    .Online
+            );
+        }
+
+        [TestMethod]
+        public void DotaGameCoordinatorNoError()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                "No Error",
+                response
+                    .SteamGameCoordinationInterface
+                    .DotaGameCoordinatorInterface
+                    .Error
+            );
+        }
+
+        [TestMethod]
+        public void DotaGameCoordinatorPlayersSearching()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                26629,
+                response
+                    .SteamGameCoordinationInterface
+                    .DotaGameCoordinatorInterface
+                    .Statistics
+                    .PlayersSearching
+            );
+        }
+
+        [TestMethod]
+        public void CounterStrikeGlobalOffensiveGameCoordinatorIsOnline()
+        {
+            var response = _client.Get();
+            Assert.IsTrue(
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Online
+            );
+        }
+
+        [TestMethod]
+        public void CounterStrikeGlobalOffensiveGameCoordinatorNoError()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                "No Error",
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Error
+            );
+        }
+
+        [TestMethod]
+        public void CounterStrikeGlobalOffensiveGameCoordinatorPlayerSearching()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                10691,
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Statistics
+                    .PlayersSearching
+            );
+        }
+
+        [TestMethod]
+        public void CounterStrikeGlobalOffensiveGameCoordinatorAverageWait()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                71788,
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Statistics
+                    .AverageWaitTime
+            );
+        }
+
+        [TestMethod]
+        public void CounterStrikeGlobalOffensiveGameCoordinatorOngoingMatches()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                28871,
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Statistics
+                    .OnGoingMatches
+            );
+        }
+
+        [TestMethod]
+        public void
+            CounterStrikeGlobalOffensiveGameCoordinatorServersAvailable()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                158632,
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Statistics
+                    .ServersAvailable
+            );
+        }
+
+        [TestMethod]
+        public void
+            CounterStrikeGlobalOffensiveGameCoordinatorServersOnline()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                251099,
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Statistics
+                    .ServersOnline
+            );
+        }
+
+        [TestMethod]
+        public void CounterStrikeGlobalOffensiveGameCoordinatorMenuUrl()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                "",
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Statistics
+                    .MenuUrl
+            );
+        }
+
+        [TestMethod]
+        public void CounterStrikeGlobalOffensiveGameCoordinatorPlayersOnline()
+        {
+            var response = _client.Get();
+            Assert.AreEqual(
+                615861,
+                response
+                    .SteamGameCoordinationInterface
+                    .CounterStrikeGlobalOffensiveGameCoordinatorInterface
+                    .Statistics
+                    .PlayersOnline
             );
         }
     }
